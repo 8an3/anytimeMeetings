ab231b365c50e3ee6df0ad33c3a0b3076c3e8d73
8an3 <skylerzanth@gmail.com>
2021-11-03 00:04:30 -0400

    first

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // Require the Bolt package (github.com/slackapi/bolt)
 const { App } = require("@slack/bolt");
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
 });
 
 
@@ -17,7 +18,34 @@ const app = new App({
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log(`⚡️ Bolt app is running on ${port}!`);
 })();
 
-//"start": "node app.js"%                                              
+app.event('app_mention', async ({ event, context, client, say }) => {
+  try {
+    await say({"blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Thanks for the mention <@${event.user}>! Here's a button`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Button",
+            "emoji": true
+          },
+          "value": "click_me_123",
+          "action_id": "first_button"
+        }
+      }
+    ]});
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+                                            
